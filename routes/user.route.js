8fc33f0ecb97a980c512bca0e1e60b285546ff57
Glitch (freckle-foregoing-glove) <none>
2020-05-05 17:14:53 +0000

    🚟🚤 Updated with Glitch

---
+++ routes/user.route.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const controller = require('../controllers/user.controller.js');
+const validation = require('../validation/user.validate.js');
+
+const router = express.Router();
+
+router.get('/', controller.index);
+
+router.get('/create', controller.create);
+
+router.post('/create', validation.postCreate, controller.postCreate);
+
+router.get('/edit-user/:userId', controller.edit);
+
+router.post('/edit-user/:userId', controller.postEdit);
+
+router.get('/delete/:userId', controller.delete);
+
+module.exports = router;
